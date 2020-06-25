8d3aeb547c7e74bca92c8ae7461ff43a7053a853
Benjamin <bmarquiscom@gmail.com>
2020-06-25 09:32:31 -0400

    Graphql test query

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,22 +2,43 @@ import Layout from 'components/layout/Layout';
 import SEO from 'components/layout/Seo';
 import Astronaute from 'components/shared/Astronaute';
 import { Link } from 'gatsby';
+import useHttp from 'hooks/http';
 import React from 'react';
 
+const query = `
+query {
+    users(orderBy: id) {
+    
+      tokens(orderBy: id) {
+        device
+      }
+    }
+}
+`;
+
 // The home page
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi there!</h1>
-    <p>Welcome to your PWA boilerplate.</p>
-    <div style={{
-      maxWidth: '300px', marginBottom: '1.45rem'
-    }}
-    >
-      <Astronaute />
-    </div>
-    <Link to="/app/profile/">Go to page 2</Link>
-  </Layout>
-);
+const IndexPage = () => {
+
+  const { httpData } = useHttp({
+    url: '/graphql', method: 'POST', body: { query }
+  });
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hi there!</h1>
+      {JSON.stringify(httpData)}
+      <p>Welcome to your PWA boilerplate.</p>
+      <div style={{
+        maxWidth: '300px', marginBottom: '1.45rem'
+      }}
+      >
+        <Astronaute />
+      </div>
+      <Link to="/app/profile/">Go to page 2</Link>
+    </Layout>
+  );
+
+};
 
 export default IndexPage;
